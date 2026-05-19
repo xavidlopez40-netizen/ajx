ffa8b103ca5e26a0eeeeea75d1c29e37f47b2c61
xavidlopez40-netizen <xavidlopez40@gmail.com>
2026-05-18 18:20:10 -0600

    Add files via upload

---
+++ lunes2.py
@@ -0,0 +1,6 @@
+def saludar(nombre):
+    print("Hola,", nombre, "bienvenido al mundo de alicia el pais de las maravillas")
+
+nombre_usuario = input("Ingresa tu nombre: ")
+saludar(nombre_usuario)
+
